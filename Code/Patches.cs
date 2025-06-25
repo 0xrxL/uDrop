53012f49296a83c74f779bdf5b380b6fac0a3657
0xrxL <oiz92@outlook.com>
2025-06-25 06:49:57 +0200

    feat: Applied timeout to core threads too

--- Code/Patches.cs
+++ Code/Patches.cs
@@ -4115,7 +4115,7 @@
                                                     i,
 
                                                     [
-                                                        "const/4 v1, 0x1",
+                                                        "const/4 v1, 0x0",
                                                         "const/4 v2, 0x1",
                                                         "const-wide/16 v3, 0x1"
                                                     ])
